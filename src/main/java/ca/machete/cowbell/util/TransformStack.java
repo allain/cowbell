1e070947cdaec13369894704e2c275d96f8b6089
Allain Lalonde <allain@allain-mini.(none)>
2010-01-29 22:01:02 -0500

    Making paint make user of Transform Stack

---
+++ src/main/java/ca/machete/cowbell/util/TransformStack.java
@@ -0,0 +1,32 @@
+package ca.machete.cowbell.util;
+
+import java.awt.geom.AffineTransform;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransformStack {
+
+  private List<AffineTransform> transforms;
+
+  public TransformStack() {
+    transforms = new ArrayList<AffineTransform>();
+  }
+
+  public void push(AffineTransform transform) {
+    if (transforms == null)
+      throw new IllegalArgumentException("Null transform invalid on stack");
+
+    transforms.add(transform);
+  }
+
+  public AffineTransform pop() {
+    return transforms.remove(transforms.size() - 1);
+  }
+
+  public AffineTransform peek() {
+    if (transforms.isEmpty())
+      return null;
+
+    return transforms.get(transforms.size() - 1);
+  }
+}
