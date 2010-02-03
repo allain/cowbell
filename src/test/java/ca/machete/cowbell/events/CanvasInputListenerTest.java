8ebc012dbc6d73710db64c257adcc505bed3eba9
Allain Lalonde <allain@allain-mini.(none)>
2010-02-03 08:20:57 -0500

    Cleaned up test and pushed coverage back up to 100%

--- src/test/java/ca/machete/cowbell/events/CanvasInputListenerTest.java
+++ src/test/java/ca/machete/cowbell/events/CanvasInputListenerTest.java
@@ -38,113 +38,122 @@ public class CanvasInputListenerTest {
     @Test
     public void mouseClickedDispatchesToHierarchy() {
         MouseEvent leftClick = new SimpleMouseEvent(MouseEvent.MOUSE_CLICKED, 0, 0);
-        final int clickCount[] = new int[1];
 
-        IMouseListener countingListener = new MouseAdapter() {
-
-            @Override
-            public void mouseClicked(IMouseEvent mouseEvent) {
-                clickCount[0]++;
-            }
-        };
-
-        root.addMouseListener(countingListener);
-        layer.addMouseListener(countingListener);
-        layer.setSize(100, 100);
+        CountingListener countingListener = buildSceneAndReturnCountingListener();
 
         listener.mouseClicked(leftClick);
-        
-        assertEquals(2, clickCount[0]);
+
+        assertEquals(2, countingListener.getClickCount());
     }
-    
+
     @Test
     public void mousePressedDispatchesToHierarchy() {
-        final int pressCount[] = new int[1];
-
-        IMouseListener countingListener = new MouseAdapter() {
-
-            @Override
-            public void mousePressed(IMouseEvent mouseEvent) {
-                pressCount[0]++;
-            }
-        };
-
-        root.addMouseListener(countingListener);
-        layer.addMouseListener(countingListener);
-        layer.setSize(100, 100);
+        CountingListener countingListener = buildSceneAndReturnCountingListener();
 
         listener.mousePressed(new SimpleMouseEvent(MouseEvent.MOUSE_PRESSED, 0, 0));
-        
-        assertEquals(2, pressCount[0]);
+
+        assertEquals(2, countingListener.getPressCount());
     }
 
     @Test
     public void mouseReleasedDispatchesToHierarchy() {
-        final int releaseCount[] = new int[1];
-
-        IMouseListener countingListener = new MouseAdapter() {
-
-            @Override
-            public void mouseReleased(IMouseEvent mouseEvent) {
-                releaseCount[0]++;
-            }
-        };
-
-        root.addMouseListener(countingListener);
-        layer.addMouseListener(countingListener);
-        layer.setSize(100, 100);
+        CountingListener countingListener = buildSceneAndReturnCountingListener();
 
         listener.mouseReleased(new SimpleMouseEvent(MouseEvent.MOUSE_RELEASED, 0, 0));
-        
-        assertEquals(2, releaseCount[0]);
+
+        assertEquals(2, countingListener.getReleaseCount());
     }
-    
+
     @Test
     public void mouseMovedDispatchesToHierarchy() {
-        final int releaseCount[] = new int[1];
-
-        IMouseListener countingListener = new MouseAdapter() {
-
-            @Override
-            public void mouseMoved(IMouseEvent mouseEvent) {
-                releaseCount[0]++;
-            }
-        };
-
-        root.addMouseListener(countingListener);
-        layer.addMouseListener(countingListener);
-        layer.setSize(100, 100);
+        CountingListener countingListener = buildSceneAndReturnCountingListener();
 
         listener.mouseMoved(new SimpleMouseEvent(MouseEvent.MOUSE_MOVED, 0, 0));
-        
-        assertEquals(2, releaseCount[0]);
+
+        assertEquals(2, countingListener.getMoveCount());
     }
 
-    
     @Test
     public void mouseDraggedDispatchesToHierarchy() {
-        final int releaseCount[] = new int[1];
+        CountingListener countingListener = buildSceneAndReturnCountingListener();
 
-        IMouseListener countingListener = new MouseAdapter() {
+        listener.mouseDragged(new SimpleMouseEvent(MouseEvent.MOUSE_DRAGGED, 0, 0));
 
-            @Override
-            public void mouseMoved(IMouseEvent mouseEvent) {
-                releaseCount[0]++;
-            }
-        };
+        assertEquals(2, countingListener.getMoveCount());
+    }
+
+    private CountingListener buildSceneAndReturnCountingListener() {
+        CountingListener countingListener = new CountingListener();
 
         root.addMouseListener(countingListener);
         layer.addMouseListener(countingListener);
         layer.setSize(100, 100);
-
-        listener.mouseDragged(new SimpleMouseEvent(MouseEvent.MOUSE_DRAGGED, 0, 0));
-        
-        assertEquals(2, releaseCount[0]);
+        return countingListener;
+    }
+    
+    @Test
+    public void mouseEnteredDoesNothing() {
+        listener.mouseEntered(null);
     }
 
+    @Test
+    public void mouseExitedDoesNothing() {
+        listener.mouseExited(null);
+    }
+
+    @SuppressWarnings("serial")
     private class SimpleMouseEvent extends MouseEvent {
+
         public SimpleMouseEvent(int type, int x, int y) {
-            super(canvas, type, System.currentTimeMillis(), InputEvent.BUTTON1_MASK, x,y, 1, false, MouseEvent.BUTTON1);
+            super(canvas, type, System.currentTimeMillis(), InputEvent.BUTTON1_MASK, x, y, 1, false, MouseEvent.BUTTON1);
         }
+    }
+
+    private static class CountingListener implements IMouseListener {
+
+        private int clickCount;
+
+        private int pressCount;
+
+        private int releaseCount;
+
+        private int moveCount;
+
+        @Override
+        public void mouseClicked(IMouseEvent mouseEvent) {
+            clickCount++;
+        }
+
+        @Override
+        public void mouseMoved(IMouseEvent mouseEvent) {
+            moveCount++;
+        }
+
+        @Override
+        public void mousePressed(IMouseEvent mouseEvent) {
+            pressCount++;
+        }
+
+        @Override
+        public void mouseReleased(IMouseEvent mouseEvent) {
+            releaseCount++;
+        }
+
+        public int getClickCount() {
+            return clickCount;
+        }
+
+        public int getPressCount() {
+            return pressCount;
+        }
+
+        public int getReleaseCount() {
+            return releaseCount;
+        }
+
+        public int getMoveCount() {
+            return moveCount;
+        }
+
     }
 }
